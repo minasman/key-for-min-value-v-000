2defcada33bab06703f3a4ec50d37b718c64c628
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-20 17:53:53 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,6 +4,7 @@
 def key_for_min_value(name_hash)
   return nil if name_hash == {}
   min_number = ""
+  the_key = ""
   name_hash.each do |name, number|
     if min_number == ""
       min_number = number
